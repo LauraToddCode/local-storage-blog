b6b1333bddc917d456a52ba9a2fb1cba8647c5d8
LauraToddCode <laura20todd@gmail.com>
2021-02-28 12:24:31 +0000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            name: "",
+            username: ""
+        }
+        this.handleNameChange = this.handleNameChange.bind(this);
+        this.handleUsernameChange = this.handleUsernameChange.bind(this);
+        this.handleFormSubmit = this.handleFormSubmit.bind(this);
+    }
+
+    handleNameChange = (event) => {
+        this.setState({
+            name : event.target.value
+        })
+    }
+
+    handleUsernameChange = (event) => {
+        this.setState({
+            username : event.target.value
+        })
+    }
+
+    handleFormSubmit = () => {
+        const {name, username} = this.state;
+        localStorage.setItem("name", name);
+        localStorage.setItem("username", username);
+    }
+
+    componentDidMount() {
+        const name = localStorage.getItem("name");
+        const username = localStorage.getItem("username");
+        this.setState({ name, username });
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleFormSubmit}>
+                <label>Name:</label>
+                <input type="text" value={this.state.name} onChange={this.handleNameChange}></input>
+                <label>Username:</label>
+                <input type="text" value={this.state.username} onChange={this.handleUsernameChange}></input>
+                <button type="submit">Submit</button>
+            </form>
+        )
+    }
 }
+
 
 export default App;
