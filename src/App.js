71004401452c60f404473646955819f3d3d541d8
LauraToddCode <laura20todd@gmail.com>
2021-02-28 14:11:07 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,37 +3,6 @@ import React from "react";
 class App extends React.Component {
     constructor(props) {
         super(props)
-        this.state = {
-            name: "",
-            username: ""
-        }
-        this.handleNameChange = this.handleNameChange.bind(this);
-        this.handleUsernameChange = this.handleUsernameChange.bind(this);
-        this.handleFormSubmit = this.handleFormSubmit.bind(this);
-    }
-
-    handleNameChange = (event) => {
-        this.setState({
-            name : event.target.value
-        })
-    }
-
-    handleUsernameChange = (event) => {
-        this.setState({
-            username : event.target.value
-        })
-    }
-
-    handleFormSubmit = () => {
-        const {name, username} = this.state;
-        localStorage.setItem("name", name);
-        localStorage.setItem("username", username);
-    }
-
-    componentDidMount() {
-        const name = localStorage.getItem("name");
-        const username = localStorage.getItem("username");
-        this.setState({ name, username });
     }
 
     render() {
